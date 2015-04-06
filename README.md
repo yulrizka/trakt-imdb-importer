233113137743d3d01142ed26f754087ce587dd49
Ahmy Yulrizka <ahmy@sense-os.nl>
2015-04-06 21:17:23 +0200

    Initial working version

---
+++ README.md
@@ -0,0 +1,63 @@
+# Trakt IMDB importer
+
+[Trakt.tv](http://trakt.tv/) is a service where you can subscribe, rate, comments movies or TV shows. It also have integration with some multimedia app such as XBMC, KODI, etc. 
+
+This script will import ratings from IMDB csv output and import it to Trakt.tv using their v2 API via OAuth.
+
+Warning: The author doesn't guarantee anything including the outcome of the running the script. use at your own risk!.
+
+## Getting IMDB csv file.
+
+1. Login to IMDB
+2. Form "Your Name" drop down, select "Your Lists"
+3. Select "Your Ratings" list
+4. At the bottom of the page there is link "Export this list"
+5. Save the file on your computer
+
+## Using the script
+
+### installing
+
+clone the repository
+
+**Make sure you have ruby * bundler installed**
+
+Most osx already came with ruby installed. to check 
+
+    $ ruby -v
+
+to install [bundler](http://bundler.io/)
+
+    $ gem install bundler 
+
+    # or with sudo
+
+    $ sudo gem install bundler
+
+
+after bundler is install, download dependency with
+
+    $ bundle
+
+### Create Trakt.tv application
+
+In order to use OAUTH, you need to create an application on trakt.tv
+
+1. Go to  [Create Application on trakt.tv](https://trakt.tv/oauth/applications/new)
+2. Enter Name & Description as you wish
+3. Enter Redirect uri: `urn:ietf:wg:oauth:2.0:oob`
+4. Tick checking & scrobble
+5. Copy "Client ID" & "Client Secret"
+
+### Run the application
+
+suppose you have the csv file name 'ratings-imdb.csv' in the same directory as the project
+
+execute 
+
+    $ bundle exec ruby import.rb ratings-imdb.csv
+
+1. it will ask for "Client ID" and "Client Secreet" you obtain erlier. Copy paste (and enter) for each value
+2. It will then ask you to open link in the browser. Login with your account and authorize the app.
+3. Copy the "OAUTH AUTHORIZATION CODE" to the terminal
+4. It will then post all the reatings & history (mark as watched) data from IMDB to trakt.
---
+++ import.rb
@@ -0,0 +1,106 @@
+require 'oauth2'
+require 'csv'
+
+##
+# Configuration
+##
+
+# enter "Client ID" and "Client Secret" here to avoid typing it multiple time
+client_id = nil
+client_secret = nil
+
+if ARGV.count < 1
+  puts
+  puts "  use ruby import.rb imdb-filename.csv"
+  puts
+  exit 1
+end
+
+if client_id.nil?
+  print "Client ID : "
+  client_id = STDIN.gets.chomp
+end
+
+if client_secret.nil?
+  print "Client Secret : "
+  client_secret = STDIN.gets.chomp
+end
+
+##
+# Generate Session token
+##
+client =  OAuth2::Client.new(client_id, client_secret, :token_url => '/oauth/token', :site =>'https://api-v2launch.trakt.tv')
+
+
+## GET access token and ask user to copy
+auth_url = client.auth_code.authorize_url(:redirect_uri => 'urn:ietf:wg:oauth:2.0:oob')
+
+puts
+puts "Open in browser:"
+puts auth_url
+puts
+print "OAuth Authorization Code : "
+auth_token = STDIN.gets.chomp
+
+token = client.auth_code.get_token(auth_token, :redirect_uri => 'urn:ietf:wg:oauth:2.0:oob')
+
+headers = {
+  'trakt-api-version' =>  '2',
+  'trakt-api-key' =>  client_id,
+  'Content-Type' => 'application/json'
+}
+
+##
+# Read from IMDB csv output and send in batch of 20 entries
+##
+csv = CSV.read(ARGV[0], headers: true)
+
+nbatch = 20
+total_record = csv.count
+
+i = 0
+uploaded = 0
+csv.each_slice(20) do |batch|
+
+  movies = []
+  shows = []
+
+  batch.each do |row|
+    entry = {
+      "rated_at" => Time.parse(row["created"]).strftime("%FT%T"),
+      "rating"   => row["You rated"],
+      "title"    => row["Title"],
+      "year"     => row["Year"],
+      "ids"      => {
+        "imdb"   => row["const"]
+      }
+    }
+
+    (row["Title type"] == "TV Series" ?  shows : movies).push entry
+  end
+  
+  request = {
+    body: {movies: movies, shows:shows}.to_json,
+    headers: headers
+  }
+
+  # synchronize ratings
+  response_ratings = token.post('sync/ratings', request)
+
+  # synchronize watched
+  response_history = token.post('sync/history', request)
+
+  if response_ratings && response_ratings.status == 201 && response_history && response_history.status == 201
+    # success
+    batch.each do |entry|
+      puts "#{entry['const']} - #{entry['Year']} #{entry['Title']} -> #{entry['You rated']}"
+    end
+  else
+    puts "There is some error while sync data"
+    puts "sync ratings resposne: #{response_ratings.status}, sync history response: #{response_history.status}"
+  end
+
+  i += 1
+  uploaded += batch.count
+  puts "Uploaded #{uploaded}, #{(1.0 * i * nbatch / total_record * 100).to_i} %"
+end
